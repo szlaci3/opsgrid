832e0d33ac2be9f90f0078874f179fa7b903b8a2
Szalai Laci <szalai_laci@yahoo.com>
2026-06-08 09:46:56 +0200

    Ornaments, favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import { CasesPage } from "./features/cases/CasesPage";
 import styles from "./app/App.module.css";
+import topLeft from './assets/topLeft.png';
+import bottomLeft from './assets/bottomLeft.png';
+import bottomRight from './assets/bottomRight.png';
 
 function App() {
   return (
     <div className={styles.shell}>
+      <div className={styles.decorLayer}>
+        <img
+          src={topLeft}
+          className={styles.topLeft}
+          alt=""
+        />
+
+        <img
+          src={bottomLeft}
+          className={styles.bottomLeft}
+          alt=""
+        />
+
+        <img
+          src={bottomRight}
+          className={styles.bottomRight}
+          alt=""
+        />
+      </div>
+
       <CasesPage />
     </div>
   );
