a5efb7298c26e93b608ab1db4b00d98ae884a24f
Szalai Laci <szalai_laci@yahoo.com>
2026-08-14 23:20:22 +0200

    exclude playwright tests from suit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
+    include: ['src/**/*.{test,spec}.{ts,tsx}'],
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setupTests.ts',
